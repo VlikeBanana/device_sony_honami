5b8b513061c980bc18bae60d4e78501b332c975f
Andreas Fried <AndrolinRoot@googlemail.com>
2014-07-18 20:17:35 +0200

    CM Defconfig for now
    will add carbon kernel later

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := vu_rhine_honami_row_defconfig
+TARGET_KERNEL_CONFIG := cm_rhine_honami_row_defconfig
 SOMC_DTS_TARGET := rhine_honami_row
 
 # External apps on SD
